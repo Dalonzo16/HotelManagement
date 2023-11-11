ed422d6d0d1a9a28c3c735ac5cf57e0f2d91c1df
Ludwig Scherer <schererludwig@gmail.com>
2023-11-10 19:49:54 -0700

    Restructured

--- HotelManagement.java
+++ HotelManagement.java
@@ -1,47 +1,11 @@
 import java.util.Map;
 
-
-
 /**
  *This program is a hotel management program
  * @author Devon
  */
 public class HotelManagement 
 {
-    /**
-     * This method calls the methods from login menu to execute and recieves a hotel and an object login
-     * @param login an object of LoginMenu
-     * @param hotel an object of Hotel
-     */
-    public static void Login(LoginMenu login, Hotel hotel)
-    {
-        do
-        {//do while loop to keep asking user for input until info is correct
-            login.displayLogin();
-            login.checkUserCredentials(hotel.getEmployees());
-        }
-        while(login.isValid() == false);
-    }
-    /**
-     * This method calls the main menu methods to display the menu options and take in the inputs
-     * @param aMenu an object of Menu
-     * @param option the quit option for the main menu
-     * @param rooms a list of rooms
-     * @param employees a list of employees
-     */
-    public static void menus(Menu aMenu, byte option, Map<Integer, Room> rooms, Map<Integer, Employee> employees)
-    {
-        do
-        {
-            aMenu.displayMenu();
-            aMenu.checkInput(rooms, employees);
-        }
-        while(aMenu.getUserSelection() != option);
-    }
-    /**
-     * This is the main method
-     * @param args a list of command line arguments
-     */
     public static void main(String[] args) 
     {    
         Hotel mainHotel = new Hotel("Best Hotel Ever"); //created hotel object
@@ -57,9 +21,7 @@ public class HotelManagement
         Room room1 = new Room(101, 50.00);//created room
         mainHotel.addRoom(room1);//added room to hotel
 
-        LoginMenu login = new LoginMenu();//created login object
-        Login(login,mainHotel); // calling methods created above to execute
-        Menu main = new Menu();
-        menus(main, main.getQuitOption(), mainHotel.getRooms(), mainHotel.getEmployees());
+        InitialLogin initialLogin = new InitialLogin();//created initial login object
+        initialLogin.displayMenu(mainHotel.getEmployees());//displayed initial login menu
     }
 }
